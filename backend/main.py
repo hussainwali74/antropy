a02d0b12c0dac19cf702132804ab30a3d6bdb95d
Hussain Wali <hussainwali743@gmail.com>
2025-10-26 23:05:07 +0100

    setup

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from grocery-system!")
 
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {"message":"hello word"}
